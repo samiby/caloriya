b724389d63ca7932bd5f044eefe754017e6e5d0e
Sami Benyoussef <sami.benyoussef@hotmail.de>
2019-03-14 22:03:59 +0100

    Initial Release Files

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# caloriya
+# Caloriya
+
+Caloriya est une application mobile simple, pratique, modulable & extensible pour celles ou ceux qui veulent calculer leurs besoins journaliers en calories.
+
+<a href='https://play.google.com/store/apps/details?id=com.caloriya.sami&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Disponible sur Google Play' src='https://play.google.com/intl/fr_FR/badges/images/generic/fr_badge_web_generic.png'/></a>
+Google Play et le logo Google Play sont des marques de Google LLC.
+
+# Vidéo de Presentation:
+Une Vidéo de Présentation est désormais disponible ici: https://youtu.be/OTqbDAbXWx0
+
+# Licence
+
+The MIT License (MIT)
+
+Copyright (c) 2019 Sami Benyoussef
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ lib/main.dart
@@ -0,0 +1,356 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: 'Caloriya',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Caloriya'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  int calorieBase;
+  int calorieAvecActivite;
+  int radioSelectionnee;
+  double poids;
+  double age;
+  bool genre = false;
+  double taille = 170.0;
+  Map mapActivite = {
+
+    0: "Faible",
+    1: "Modere",
+    2: "Forte"
+  };
+
+  /* Begin App Exit Alert Dialog */
+  Future<bool> _onWillPop() {
+    return showDialog(
+      context: context,
+      builder: (context) => new AlertDialog(
+        title: new Text('Quitter Caloriya'),
+        content: new Text('Voulez vous vraiment quitter cette application?'),
+        actions: <Widget>[
+          new FlatButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: new Text('Non'),
+          ),
+          new FlatButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: new Text('Oui'),
+          ),
+        ],
+      ),
+    ) ?? false;
+  }
+  /* End App Exit Alert Dialog */
+
+  @override
+  Widget build(BuildContext context) {
+
+    return new WillPopScope(
+        onWillPop: _onWillPop,
+    /* return new GestureDetector( */
+    child: new GestureDetector(
+      onTap: (() => FocusScope.of(context).requestFocus(new FocusNode())),
+
+      child: new Scaffold(
+
+        appBar: new AppBar(
+          title: new Text(widget.title),
+          backgroundColor: setColor(),
+        ),
+
+        body: new SingleChildScrollView(
+          padding: EdgeInsets.all(15.0),
+
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+            children: <Widget>[
+              padding(),
+              texteAvecStyle("Remplissez tous les champs pour obtenir votre besoin journalier en calories."),
+              padding(),
+              new Card(
+                elevation: 10.0,
+                child: new Column(
+                  children: <Widget>[
+                    padding(),
+                    new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        texteAvecStyle("Femme", color: Colors.pink),
+                        new Switch(
+                            value: genre,
+                            inactiveTrackColor: Colors.pink,
+                            activeTrackColor: Colors.blue,
+                            onChanged: (bool b) {
+                              setState(() {
+                                genre = b;
+                              });
+                            }),
+                        texteAvecStyle("Homme", color: Colors.blue)
+                      ],
+                    ),
+                    padding(),
+                    new RaisedButton(
+                        color: setColor(),
+                        child: texteAvecStyle((age == null)? "Appuyez pour entrer votre age": "Votre age est de : ${age.toInt()}",
+                            color: Colors.white
+                        ),
+                        onPressed: (() => montrerPicker())
+                    ),
+                    padding(),
+                    texteAvecStyle("Votre taille est de: ${taille.toInt()} cm.", color: setColor()),
+                    padding(),
+                    new Slider(
+                      value: taille,
+                      activeColor: setColor(),
+                      onChanged: (double d) {
+                        setState(() {
+                          taille = d;
+                        });
+                      },
+                      max: 215.0,
+                      min: 100.0,
+                    ),
+                    padding(),
+                    padding(),
+
+                    new TextField(
+
+                      textAlign: TextAlign.start,
+                      keyboardType: TextInputType.number,
+                      onChanged: (String string) {
+                        setState(() {
+                          poids = double.tryParse(string);
+                        });
+                      },
+
+                      // TODO: Essayer de centrer le texte
+                      decoration: new InputDecoration(
+                          //fillColor: Colors.grey[500],
+                          labelText: "Entrez votre poids en kilos."
+                      ),
+                      // TODO: Essayer de centrer le texte
+
+                    ),
+
+
+                    padding(),
+                    texteAvecStyle("Quelle est votre activité sportive?", color: setColor()),
+                    padding(),
+                    rowRadio(),
+                    padding()
+                  ],
+                ),
+              ),
+              padding(),
+              new RaisedButton(
+                color: setColor(),
+                child: texteAvecStyle("Calculer", color: Colors.white),
+                onPressed: calculerNombreDeCalories,
+
+              )
+
+            ],
+
+          ),
+
+        ),
+
+      ),
+
+    ),
+
+    );
+
+  }
+
+
+  Padding padding() {
+    return new Padding(padding: EdgeInsets.only(top: 20.0));
+  }
+
+  Future<Null> montrerPicker() async {
+    DateTime choix = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(1900),
+        lastDate: new DateTime.now(),
+        initialDatePickerMode: DatePickerMode.year
+    );
+    if (choix != null) {
+      var difference = new DateTime.now().difference(choix);
+      var jours = difference.inDays;
+      var ans = (jours / 365);
+      setState(() {
+        age = ans;
+      });
+    }
+  }
+
+
+  Color setColor() {
+    if (genre) {
+      return Colors.blue;
+    } else {
+      return Colors.pink;
+    }
+  }
+
+  Text texteAvecStyle(String data, {color: Colors.black, fontSize: 15.0}) {
+    return new Text(
+        data,
+        textAlign: TextAlign.center,
+        style: new TextStyle(
+            color: color,
+            fontSize: fontSize
+        )
+    );
+  }
+
+  Row rowRadio() {
+    List<Widget> l = [];
+    mapActivite.forEach((key, value) {
+      Column colonne = new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          new Radio(
+              activeColor: setColor(),
+              value: key,
+              groupValue: radioSelectionnee,
+              onChanged: (Object i) {
+                setState(() {
+                  radioSelectionnee = i;
+
+                });
+              }),
+          texteAvecStyle(value, color: setColor())
+        ],
+      );
+      l.add(colonne);
+    });
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: l,
+    );
+  }
+
+  void calculerNombreDeCalories() {
+    if (age != null && poids != null && radioSelectionnee != null) {
+      //Calculer
+      if (genre) {
+        calorieBase = (66.4730 + (13.7516 * poids) + (5.0033 * taille) - (6.7550 * age)).toInt();
+      } else {
+        calorieBase = (655.0955 + (9.5634 * poids) + (1.8496 * taille) - (4.6756 * age)).toInt();
+      }
+      switch(radioSelectionnee) {
+        case 0:
+          calorieAvecActivite = (calorieBase * 1.2).toInt();
+          break;
+        case 1:
+          calorieAvecActivite = (calorieBase * 1.5).toInt();
+          break;
+        case 2:
+          calorieAvecActivite = (calorieBase * 1.8).toInt();
+          break;
+        default:
+          calorieAvecActivite = calorieBase;
+          break;
+      }
+
+      setState(() {
+        dialogue();
+      });
+
+    } else {
+      alerte();
+    }
+  }
+
+  Future<Null> dialogue() async {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext buildContext) {
+          return SimpleDialog(
+            title: texteAvecStyle("Votre besoin en calories", color: setColor()),
+            contentPadding: EdgeInsets.all(15.0),
+            children: <Widget>[
+              padding(),
+              texteAvecStyle("Votre besoin de base est de: $calorieBase"),
+              padding(),
+              texteAvecStyle("Votre besoin avec activité sportive est de : $calorieAvecActivite"),
+              new RaisedButton(onPressed: () {
+                Navigator.pop(buildContext);
+              },
+                child: texteAvecStyle("OK", color: Colors.white),
+                color: setColor(),
+              )
+            ],
+          );
+        }
+    );
+  }
+
+  Future<Null> alerte() async {
+    return showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext buildContext) {
+          return new AlertDialog(
+            title: texteAvecStyle("Erreur"),
+            content: texteAvecStyle("Tous les champs ne sont pas remplis"),
+            actions: <Widget>[
+              new FlatButton(
+                  onPressed: () {
+                    Navigator.pop(buildContext);
+                  },
+                  child: texteAvecStyle("OK", color: Colors.red))
+            ],
+          );
+        }
+    );
+  }
+
+}
